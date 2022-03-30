23552a0c08acba9e9fee390017b2d6f9d0fc2fd0
anmitalee <96329523+anmitalee@users.noreply.github.com>
2022-03-30 13:31:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var ready = (callback) => {
 }
 
 ready(() => { 
-	document.querySelector(".header").style.height = window.innerHeight + "px";
+	document.querySelector(".header").style.height = window.innerHeight/3 + "px";
 })
